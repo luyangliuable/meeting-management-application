f88c1d776f892dac98c5808a4f56adccacb3f59e
Luyang Liu <rubber@Luyangs-MacBook-Pro.local>
2021-07-14 22:21:44 +1000

    made toggle button functional and nearly made side bar and JoinMeeting function workng

--- frontend/my-app/src/components/cards.js
+++ frontend/my-app/src/components/cards.js
@@ -1,18 +1,18 @@
 import Task from './card'
 import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd'
 
-const Tasks = ({ tasks, joinMeeting, onDelete, onToggle }) => {
+const Tasks = ({ tasks, participants, setParticipants, func, onDelete, onToggle }) => {
     return (
             <DragDropContext>
                 <Droppable droppableId = 'agendalist'>
-            {(provided) => (
-                    <ul {...provided.droppableProps} ref={provided.innerRef}>
-                        {tasks.map((tasks, index) => (
-                            <Task tasks={tasks} joinMeeting="joinMeeting()" />
-                        ))}
-                {provided.placeholder}
-                    </ul>
-            )}
+                    {(provided) => (
+                            <ul {...provided.droppableProps} ref={provided.innerRef}>
+                                {tasks.map((tasks, index) => (
+                                    <Task func={ func } participants={participants} setParticipants = { setParticipants } tasks={tasks} />
+                                ))}
+                        {provided.placeholder}
+                            </ul>
+                    )}
                 </Droppable>
             </DragDropContext>
     )
