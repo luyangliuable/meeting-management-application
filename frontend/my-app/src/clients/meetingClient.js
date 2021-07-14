013ea85ab4a6a91a6f737c02dd912629e046d27b
Jeremy Chrimes <jeremy.chrimes@gmail.com>
2021-07-14 19:03:53 +1000

    Finished Meeting Creation Component

--- frontend/my-app/src/clients/meetingClient.js
+++ frontend/my-app/src/clients/meetingClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export function addNewMeeting(meeting) { 
-    axios.post("localhost:8081/meeting", meeting).then((response) => {
-        return (response.status === 200) ? true : false;
+    axios.post("http://localhost:8081/meeting", meeting).then((response) => {
+        return true;
     }).catch((error) => {
         console.log(error);
         return false;
--- frontend/my-app/src/components/MeetingCreation.js
+++ frontend/my-app/src/components/MeetingCreation.js
@@ -2,28 +2,40 @@ import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import Select from  "react-select";
+import { addNewMeeting } from "../clients/meetingClient";
 
 const selectOptions = [
-    {
-        value: "Complete",
-        label: "Complete"
-    },
-    {
-        value: "In-Progress",
-        label: "In-Progress"
-    },
-    {
-        value: "Scheduled",
-        label: "Scheduled"
-    }
+{
+    value: "Scheduled",
+    label: "Scheduled"
+},
+{
+    value: "Complete",
+    label: "Complete"
+},
+{
+    value: "In-Progress",
+    label: "In-Progress"
+}
 ];
 
 export function MeetingCreation(props) {
     const [meetingName, setMeetingName] = useState();
     const [meetingDateTime, setMeetingDateTime] = useState(new Date());
+    const [meetingStatus, setMeetingStatus] = useState(selectOptions[0]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addNewMeeting({
+            meetingName: meetingName,
+            meetingDateTime: meetingDateTime,
+            meetingStatus: meetingStatus.value
+        });
+    }
     return (
         <div>
-            <form>
+            <p>{meetingStatus !== undefined ? meetingStatus.value : ""}</p>
+            <form onSubmit = {handleSubmit}>
                 <label>Meeting Name {meetingName}</label>
                 <input 
                     value  = {meetingName} 
@@ -37,9 +49,11 @@ export function MeetingCreation(props) {
                     showTimeSelect/>
                 <label>Meeting Status</label>
                 <Select
+                    defaultValue = {selectOptions}
                     options = {selectOptions}
+                    onChange = {setMeetingStatus}
                 />
-
+                <button type = "submit">Submit</button>
             </form>
         </div>
     );
